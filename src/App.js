947b16cbb5efaa6dec1d4b792d076adc8dba57cb
Kevin St.Onge <kevinstonge@gmail.com>
2020-08-16 21:29:34 -0400

    layout, sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "./App.scss";
 
 const App = () => {
+  const [formVisible, setFormVisible] = useState(false); //modal form with state true/false for visibility
+  const [order, setOrder] = useState([]); //ordered items added to array
+  const addToOrder = (item) => {
+    setOrder([...order, item]); //add item to order
+  };
+  const placeOrder = () => {
+    console.log(order);
+    axios
+      .post("https://reqres.in/api/users", order)
+      .then((r) => console.log(r))
+      .catch((e) => console.log(e)); //send order to server
+  };
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <Router>
+      <header>
+        <div>
+          <h1>Lambda Eats</h1>
+        </div>
+        <nav>
+          <Link to="/cart" onClick={() => setFormVisible(false)}>
+            cart [{order.length}]
+          </Link>
+        </nav>
+      </header>
+
+      <Switch>
+        <Route exact path="/">
+          <div className="ctaContainer">
+            <h2>hungry? click the button below to order pizza!</h2>
+            <button className="ctaButton">order pizza!</button>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 export default App;
